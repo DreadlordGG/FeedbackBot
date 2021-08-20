4f66d2c97ff57de21fb29a4a02b7249088dbacde
dreadlordgg <me@hhristov.info>
2021-08-20 15:23:48 +0200

    Init

---
+++ core/logging.py
@@ -0,0 +1,21 @@
+import logging
+import sys
+import os
+
+log_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),"../logs/", 'FeedbackBot.log')
+log_level = logging.INFO
+fh = logging.FileHandler(log_file)
+fh.setLevel(log_level)
+loggers = set()
+formatter = logging.Formatter(
+    "%(asctime)s %(name)s[%(funcName)s] - %(levelname)s: %(message)s", datefmt="%d/%m/%y %H:%M:%S"
+)
+fh.setFormatter(formatter)
+logging.setLoggerClass(logging.Logger)
+
+def getLogger(name=None, guild=None):
+    logger = logging.getLogger(name)
+    logger.setLevel(log_level)
+    logger.addHandler(fh)
+    loggers.add(logger)
+    return logger
